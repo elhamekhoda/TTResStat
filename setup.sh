23a4102f053ded65e29769916cf3f800eb77c24b
Elham E Khoda <elham.e.khoda@cern.ch>
2022-09-02 11:54:25 -0700

    adding atlas style files

--- setup.sh
+++ setup.sh
@@ -3,7 +3,6 @@
 export ATLAS_LOCAL_ROOT_BASE=/cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase
 source /cvmfs/atlas.cern.ch/repo/ATLASLocalRootBase/user/atlasLocalSetup.sh
 
-lsetup "root 6.14.04-x86_64-slc6-gcc62-opt" 
 lsetup git
 mkdir plots run
 
